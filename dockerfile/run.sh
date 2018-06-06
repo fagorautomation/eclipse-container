70297b7a4ecea9dd06af21be82262599fef8099d
Igor <igorluribravo@gmail.com>
2018-06-06 07:40:31 +0200

    -instalar dependencias necesarias para instalar SDK desde el container y tener sudo

--- dockerfile/run.sh
+++ dockerfile/run.sh
@@ -9,38 +9,45 @@ ENV_FILE=`ls /workdir/environment-setup-*`
 
 if [ -z "$ENV_FILE" ]
 then
-	echo "KK"
+	echo "SDK environment setup file not found."
 else
+	echo "setting up Yocto SDK environment."
 	. $ENV_FILE
+	
+	export LINUX_DIR="$OECORE_TARGET_SYSROOT/usr/src/kernel"
+
+	if [ ! -f $OECORE_NATIVE_SYSROOT/usr/bin/yocto-gcc ]
+	then
+		echo "Creating yocto- prefix tooclhain."
+		# Create an executable file that calls the crosscompiler with needed all parameters
+		sudo sh -c "echo '$CC \$@' > $OECORE_NATIVE_SYSROOT/usr/bin/yocto-gcc"
+		sudo chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-gcc"
+	
+		# The same for the rest tooclhain tools
+		sudo sh -c "echo '$CXX \$@' > $OECORE_NATIVE_SYSROOT/usr/bin/yocto-g++"
+		sudo chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-g++"
+		sudo sh -c "echo '$CPP \$@' > $OECORE_NATIVE_SYSROOT/usr/bin/yocto-gcpp"
+		sudo chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-gcpp"
+		sudo sh -c "echo '$AS \$@' > $OECORE_NATIVE_SYSROOT/usr/bin/yocto-as"
+		sudo chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-as"
+		sudo sh -c "echo '$LD \$@' > $OECORE_NATIVE_SYSROOT/usr/bin/yocto-ld"
+		sudo chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-ld"
+		sudo sh -c "echo '$STRIP \$@' > $OECORE_NATIVE_SYSROOT/usr/bin/yocto-strip"
+		sudo chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-strip"
+		sudo sh -c "echo '$RANLIB \$@' > $OECORE_NATIVE_SYSROOT/usr/bin/yocto-ranlib"
+		sudo chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-ranlib"
+		sudo sh -c "echo '$OBJCOPY \$@' > $OECORE_NATIVE_SYSROOT/usr/bin/yocto-objcopy"
+		sudo chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-objcopy"
+		sudo sh -c "echo '$OBJDUMP \$@' > $OECORE_NATIVE_SYSROOT/usr/bin/yocto-objdump"
+		sudo chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-objdump"
+		sudo sh -c "echo '$AR \$@' > $OECORE_NATIVE_SYSROOT/usr/bin/yocto-ar"
+		sudo chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-ar"
+		sudo sh -c "echo '$NM \$@' > $OECORE_NATIVE_SYSROOT/usr/bin/yocto-nm"
+		sudo chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-nm"
+		sudo sh -c "echo '$GDB \$@' > $OECORE_NATIVE_SYSROOT/usr/bin/yocto-gdb"
+		sudo chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-gdb"
+	fi
 fi
 
-# Create an executable file that calls the crosscompiler with needed all parameters
-echo "$CC \$@" > "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-gcc"
-chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-gcc"
-
-# The same for the rest tooclhain tools
-echo "$CXX \$@" > "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-g++"
-chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-g++"
-echo "$CPP \$@" > "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-gcpp"
-chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-gcpp"
-echo "$AS \$@" > "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-as"
-chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-as"
-echo "$LD \$@" > "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-ld"
-chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-ld"
-echo "$STRIP \$@" > "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-strip"
-chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-strip"
-echo "$RANLIB \$@" > "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-ranlib"
-chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-ranlib"
-echo "$OBJCOPY \$@" > "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-objcopy"
-chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-objcopy"
-echo "$OBJDUMP \$@" > "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-objdump"
-chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-objdump"
-echo "$AR \$@" > "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-ar"
-chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-ar"
-echo "$NM \$@" > "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-nm"
-chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-nm"
-echo "$GDB \$@" > "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-gdb"
-chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-gdb"
-
-
+# Pass through parameters
 $@
