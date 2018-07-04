bf44ce7d72ad92b06109bb97bfe67acfe9dad898
fagorautomation <iluri@aotek.es>
2018-07-04 13:51:25 +0200

    Docker based on ubuntu-base v1.01 image. Impruved entrypoint to be able to install Yocto SDKs with a special parameter.

--- dockerfile/run.sh
+++ dockerfile/run.sh
@@ -1,53 +1,59 @@
 #!/bin/sh
 
-echo "### FAGOR AUTOMATION Docker image ###"
+echo "### FAGOR AUTOMATION Eclipse Docker image for developers ##"
 
-# Find out Yocto SDK environment setup file. It asumes
-# that the SDK is installed in /workdir directory
-ENV_FILE=`ls /workdir/environment-setup-*`
-
-
-if [ -z "$ENV_FILE" ]
+if [ "$1" = "--sdk" ]
 then
-	echo "SDK environment setup file not found."
+	echo "### Installing SDK in /workdir ###"
+	/workdir/$2 -d /workdir -y
 else
-	echo "setting up Yocto SDK environment."
-	. $ENV_FILE
-	
-	export LINUX_DIR="$OECORE_TARGET_SYSROOT/usr/src/kernel"
+	# Find out Yocto SDK environment setup file. It asumes
+	# that the SDK is installed in /workdir directory
+	ENV_FILE=`ls /workdir/environment-setup-*`
 
-	if [ ! -f $OECORE_NATIVE_SYSROOT/usr/bin/yocto-gcc ]
+
+	if [ -z "$ENV_FILE" ]
 	then
-		echo "Creating yocto- prefix tooclhain."
-		# Create an executable file that calls the crosscompiler with needed all parameters
-		sudo sh -c "echo '$CC \$@' > $OECORE_NATIVE_SYSROOT/usr/bin/yocto-gcc"
-		sudo chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-gcc"
+		echo "SDK environment setup file not found."
+	else
+		echo "Setting up Yocto SDK environment."
+		. $ENV_FILE
 	
-		# The same for the rest tooclhain tools
-		sudo sh -c "echo '$CXX \$@' > $OECORE_NATIVE_SYSROOT/usr/bin/yocto-g++"
-		sudo chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-g++"
-		sudo sh -c "echo '$CPP \$@' > $OECORE_NATIVE_SYSROOT/usr/bin/yocto-gcpp"
-		sudo chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-gcpp"
-		sudo sh -c "echo '$AS \$@' > $OECORE_NATIVE_SYSROOT/usr/bin/yocto-as"
-		sudo chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-as"
-		sudo sh -c "echo '$LD \$@' > $OECORE_NATIVE_SYSROOT/usr/bin/yocto-ld"
-		sudo chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-ld"
-		sudo sh -c "echo '$STRIP \$@' > $OECORE_NATIVE_SYSROOT/usr/bin/yocto-strip"
-		sudo chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-strip"
-		sudo sh -c "echo '$RANLIB \$@' > $OECORE_NATIVE_SYSROOT/usr/bin/yocto-ranlib"
-		sudo chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-ranlib"
-		sudo sh -c "echo '$OBJCOPY \$@' > $OECORE_NATIVE_SYSROOT/usr/bin/yocto-objcopy"
-		sudo chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-objcopy"
-		sudo sh -c "echo '$OBJDUMP \$@' > $OECORE_NATIVE_SYSROOT/usr/bin/yocto-objdump"
-		sudo chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-objdump"
-		sudo sh -c "echo '$AR \$@' > $OECORE_NATIVE_SYSROOT/usr/bin/yocto-ar"
-		sudo chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-ar"
-		sudo sh -c "echo '$NM \$@' > $OECORE_NATIVE_SYSROOT/usr/bin/yocto-nm"
-		sudo chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-nm"
-		sudo sh -c "echo '$GDB \$@' > $OECORE_NATIVE_SYSROOT/usr/bin/yocto-gdb"
-		sudo chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-gdb"
-	fi
-fi
+		export LINUX_DIR="$OECORE_TARGET_SYSROOT/usr/src/kernel"
 
-# Pass through parameters
-$@
+		if [ ! -f $OECORE_NATIVE_SYSROOT/usr/bin/yocto-gcc ]
+		then
+			echo "Creating yocto- prefix tooclhain."
+			# Create an executable file that calls the crosscompiler with needed all parameters
+			sudo sh -c "echo '$CC \$@' > $OECORE_NATIVE_SYSROOT/usr/bin/yocto-gcc"
+			sudo chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-gcc"
+	
+			# The same for the rest tooclhain tools
+			sudo sh -c "echo '$CXX \$@' > $OECORE_NATIVE_SYSROOT/usr/bin/yocto-g++"
+			sudo chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-g++"
+			sudo sh -c "echo '$CPP \$@' > $OECORE_NATIVE_SYSROOT/usr/bin/yocto-gcpp"
+			sudo chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-gcpp"
+			sudo sh -c "echo '$AS \$@' > $OECORE_NATIVE_SYSROOT/usr/bin/yocto-as"
+			sudo chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-as"
+			sudo sh -c "echo '$LD \$@' > $OECORE_NATIVE_SYSROOT/usr/bin/yocto-ld"
+			sudo chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-ld"
+			sudo sh -c "echo '$STRIP \$@' > $OECORE_NATIVE_SYSROOT/usr/bin/yocto-strip"
+			sudo chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-strip"
+			sudo sh -c "echo '$RANLIB \$@' > $OECORE_NATIVE_SYSROOT/usr/bin/yocto-ranlib"
+			sudo chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-ranlib"
+			sudo sh -c "echo '$OBJCOPY \$@' > $OECORE_NATIVE_SYSROOT/usr/bin/yocto-objcopy"
+			sudo chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-objcopy"
+			sudo sh -c "echo '$OBJDUMP \$@' > $OECORE_NATIVE_SYSROOT/usr/bin/yocto-objdump"
+			sudo chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-objdump"
+			sudo sh -c "echo '$AR \$@' > $OECORE_NATIVE_SYSROOT/usr/bin/yocto-ar"
+			sudo chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-ar"
+			sudo sh -c "echo '$NM \$@' > $OECORE_NATIVE_SYSROOT/usr/bin/yocto-nm"
+			sudo chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-nm"
+			sudo sh -c "echo '$GDB \$@' > $OECORE_NATIVE_SYSROOT/usr/bin/yocto-gdb"
+			sudo chmod +x "$OECORE_NATIVE_SYSROOT/usr/bin/yocto-gdb"
+		fi
+	fi
+
+	# Pass through parameters
+	$@
+fi
